232884a55b225f9169318cf9b05f94024d376786
SanthoshKumar211299 <subjansan@gmail.com>
2024-08-31 19:57:19 +0530

    mt-6

--- src/Pages/ForgotPassword.jsx
+++ src/Pages/ForgotPassword.jsx
@@ -25,7 +25,7 @@ const ForgotPassword = () => {
   };
   return (
     <div className="d-flex justify-content-center align-items-center container mt-8">
-      <div className="bg-white p-3 mt-5 card">
+      <div className="bg-white p-3 mt-6 card">
         <legend className="fs-2 text-center ">
           <span className=" text-info"> Demo App</span>
         </legend>
