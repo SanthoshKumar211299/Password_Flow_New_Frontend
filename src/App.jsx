a1bc4bf74bf14bb3a0e6a7c3b354c8023eab5765
SanthoshKumar211299 <subjansan@gmail.com>
2024-08-31 15:09:02 +0530

    completed

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import React, { useState } from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { ToastContainer } from 'react-toastify'
+import Navbar from './Components/Navbar'
+import Home from './Pages/Home'
+import Register from './Pages/Register'
+import Login from './Pages/Login'
+import ForgotPassword from './Pages/ForgotPassword'
+import ResetPassword from './Pages/ResetPassword'
+import Footer from './Components/Footer'
+
+const App = () => {
+  const [token, setToken] = useState("");
+  return (
+    <div>
+      <BrowserRouter>
+      <ToastContainer/>
+      <Navbar/>
+      <Routes>
+        <Route path='/' element ={<Home/>}/>
+        <Route path='/register' element ={<Register/>}/>
+        <Route path='/login' element ={<Login setToken={setToken}/>}/>
+        <Route path='/forgotpassword' element ={<ForgotPassword/>}/>
+        <Route path='/resetpassword/:id/:token' element ={<ResetPassword/>}/>
+      </Routes>
+      <Footer/>
+      </BrowserRouter>
+    </div>
+  )
+}
+
+export default App
